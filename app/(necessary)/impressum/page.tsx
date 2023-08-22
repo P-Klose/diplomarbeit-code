0bf5d2ea12a6b0603e05bdfdf42bbb52b18858b4
P-Klose <ipad@klose.at>
2023-08-22 14:55:22 +0200

    feat(Impressum,Datenschutz): rename and reorder files and add Impressum and Datenschutz base files (#1, #2)

---
+++ app/(necessary)/impressum/page.tsx
@@ -0,0 +1,19 @@
+import { getStoryblokApi, StoryblokComponent } from "@storyblok/react/rsc";
+
+export default async function Home() {
+  const { data } = await fetchData();
+  return (
+    <main className="overflow-hidden">
+      <StoryblokComponent blok={data.story.content} />
+    </main>
+  );
+}
+
+export async function fetchData() {
+  let sbParams = {
+    version: process.env.storyblokApiVersion,
+  };
+
+  const stroyblokApi = getStoryblokApi();
+  return stroyblokApi.get(`cdn/stories/impressum`, sbParams);
+}
