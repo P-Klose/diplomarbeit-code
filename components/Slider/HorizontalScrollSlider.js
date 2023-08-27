5c49bda91f4614e316a09d1d6e656ce8969cf198
P-Klose <ipad@klose.at>
2023-08-27 17:11:48 +0200

    feat(Slider): add scroll on start slider

--- components/Slider/HorizontalScrollSlider.js
+++ components/Slider/HorizontalScrollSlider.js
@@ -2,19 +2,37 @@
 
 import { storyblokEditable } from "@storyblok/react/rsc";
 import { motion, useScroll, useTransform } from "framer-motion";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const Slider = ({ blok }) => {
   const targetRef = useRef();
+  const carousel = useRef();
+  const [width, setWidth] = useState(0);
   const { scrollYProgress } = useScroll({
     target: targetRef,
   });
+  useEffect(() => {
+    setWidth(
+      carousel.current.scrollWidth -
+        carousel.current.offsetWidth -
+        carousel.current.offsetWidth,
+    );
+    console.log(carousel.current);
+  }, []);
   const x = useTransform(scrollYProgress, [0, 1], ["100%", "0%"]);
   return (
     <section ref={targetRef} className="relative h-[200vh]">
       <div className="sticky top-0 flex h-screen items-center overflow-hidden p-4">
-        <div className="mx-auto w-screen max-w-screen-2xl overflow-visible bg-neutral-100">
-          <motion.div style={{ x }} className="flex gap-4">
+        <div
+          ref={carousel}
+          className="mx-auto w-screen max-w-screen-2xl overflow-visible bg-neutral-100"
+        >
+          <motion.div
+            drag="x"
+            dragConstraints={{ right: 0, left: -width }}
+            style={{ x }}
+            className={`flex gap-4 `}
+          >
             {blok.content?.map((box) => {
               return (
                 <div
