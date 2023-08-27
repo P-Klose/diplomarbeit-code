a85293ec5dae249e28df01753c537bacf5d57c45
P-Klose <ipad@klose.at>
2023-08-27 18:19:22 +0200

    feat(News): add frontend for News

---
+++ app/(dynamic content)/news/page.tsx
@@ -0,0 +1,24 @@
+import { getStoryblokApi, StoryblokComponent } from "@storyblok/react/rsc";
+import { cache } from "react";
+
+export default async function Home() {
+  const { data } = await fetchData();
+  // console.log("Fetching Data");
+  // debug(data);
+  return (
+    <main className="overflow-hidden">
+      {/* <h1>Story: {data.story.id}</h1> */}
+      <StoryblokComponent blok={data.story.content} />
+    </main>
+  );
+}
+
+export async function fetchData() {
+  let sbParams = {
+    // cache: "no-store",
+    version: process.env.storyblokApiVersion,
+  };
+
+  const stroyblokApi = getStoryblokApi();
+  return stroyblokApi.get(`cdn/stories/news`, sbParams);
+}
