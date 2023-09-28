7344cf97de7cf6cebae2f8dbf3c25041f6f59dbd
P-Klose <ipad@klose.at>
2023-09-28 20:37:52 +0200

    feat(component:Navbar): develop frontend point for only nav, and following static content (#28, #30)

---
+++ app/shared-components/[global]/page.tsx
@@ -0,0 +1,31 @@
+import { getStoryblokApi, StoryblokComponent } from "@storyblok/react/rsc";
+import { FC } from "react";
+
+interface pageProps {
+  params: { global: string };
+}
+
+const Home: FC<pageProps> = async ({ params }) => {
+  const { data } = await fetchData(params.global);
+
+  return (
+    <div>
+      <StoryblokComponent
+        blok={data.story.content}
+        key={data.story.content._uid}
+      />
+    </div>
+  );
+};
+
+export default Home;
+
+export async function fetchData(c_name: string) {
+  let sbParams = {
+    // cache: "no-store",
+    version: process.env.storyblokApiVersion,
+  };
+
+  const stroyblokApi = getStoryblokApi();
+  return stroyblokApi.get(`cdn/stories/shared-components/${c_name}`, sbParams);
+}
