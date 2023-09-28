0fec01c9b8cef171c72fda44974b221b5e861d5d
P-Klose <ipad@klose.at>
2023-09-28 20:31:26 +0200

    feat(Slider): add k-big and k-small to slider (close #22, close #23)

--- app/(tabs)/kontakt/page.tsx
+++ app/(tabs)/kontakt/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
   // console.log("Fetching Data");
   // debug(data);
   return (
-    <main className="overflow-hidden">
+    <main className="">
       {/* <h1>Story: {data.story.id}</h1> */}
       <StoryblokComponent blok={data.story.content} />
     </main>
--- components/Slider/HorizontalScrollSlider.js
+++ components/Slider/HorizontalScrollSlider.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { storyblokEditable } from "@storyblok/react/rsc";
+import { render } from "storyblok-rich-text-react-renderer";
 import { motion, useScroll, useTransform } from "framer-motion";
 import { useEffect, useRef, useState } from "react";
 
@@ -19,79 +20,117 @@ const Slider = ({ blok }) => {
         carousel.current.offsetWidth,
     );
   }, []);
-  const x = useTransform(scrollYProgress, [0, 1], ["100%", blok.scroll_width]);
+  const x = useTransform(
+    scrollYProgress,
+    [0, 1],
+    [blok.scroll_start_right ? "0%" : "100%", blok.scroll_width],
+  );
   return (
     <section
       {...storyblokEditable(blok)}
       ref={targetRef}
       className="relative h-[200vh]"
     >
-      <div className="sticky top-0 flex h-screen items-center overflow-hidden p-4">
-        <div
-          ref={carousel}
-          className="mx-6 w-screen overflow-visible bg-neutral-100"
-        >
-          <motion.div
-            drag="x"
-            dragConstraints={{ right: 0, left: -width }}
-            style={{ x }}
-            className={`flex gap-4 `}
-          >
-            {blok.content?.map((box) => {
-              if (box.type == "event") {
-                return (
-                  <div
-                    className={`border-${box.allocate} relative mr-8 flex min-w-[32rem] max-w-[32rem] flex-col items-start justify-start border-l-4`}
-                    key={box._uid}
-                  >
-                    <img
-                      className="border-box ml-2 max-w-full "
-                      src={box.main_image.filename}
-                    ></img>
-                    <h2 className="col-span-3 row-start-3 m-4 text-lg font-medium">
-                      {box.headline}
-                    </h2>
-                    <p
-                      className={`bg-${box.allocate} p-2 text-right text-xs font-normal`}
+      <div className="sticky top-0 h-screen">
+        <h1 className="px-8 pt-8 text-end text-7xl font-bold uppercase">
+          {blok.title}
+        </h1>
+        <div className="flex h-full items-center overflow-hidden p-4">
+          <div ref={carousel} className="mx-6 w-screen overflow-visible">
+            <motion.div
+              drag="x"
+              dragConstraints={{ right: 0, left: -width }}
+              style={{ x }}
+              className={`flex gap-4 `}
+            >
+              {blok.content?.map((box) => {
+                if (box.type == "event") {
+                  return (
+                    <div
+                      className={`border-${box.allocate} relative mr-8 flex min-w-[32rem] max-w-[32rem] flex-col items-start justify-start border-l-4`}
+                      key={box._uid}
                     >
-                      {box.subline}
-                    </p>
-                  </div>
-                );
-              }
+                      <img
+                        className="border-box ml-2 max-w-full "
+                        src={box.main_image.filename}
+                      ></img>
+                      <h2 className="col-span-3 row-start-3 m-4 text-lg font-medium">
+                        {box.headline}
+                      </h2>
+                      <p
+                        className={`bg-${box.allocate} p-2 text-right text-xs font-normal`}
+                      >
+                        {box.subline}
+                      </p>
+                    </div>
+                  );
+                }
 
-              if (box.type == "k-big") {
+                if (box.type == "k-big") {
+                  return (
+                    <div
+                      className="mr-12 flex min-w-[32rem] max-w-[32rem] flex-col"
+                      key={box._uid}
+                    >
+                      <div
+                        className={`mb-4 h-5 w-1/4 bg-${box.allocate}`}
+                      ></div>
+                      <h2 className="h-14 w-full text-lg uppercase">
+                        {box.headline}
+                      </h2>
+                      <div className="grid h-full grid-cols-4">
+                        <div className="col-span-2 flex h-full flex-col justify-between">
+                          <div className="flex h-1/2 flex-col justify-around">
+                            <h3>{box.subline}</h3>
+                          </div>
+                          <div className="felx flex-col justify-around">
+                            {render(box.content)}
+                          </div>
+                        </div>
+                        <div className="col-span-2">
+                          <img
+                            className="h-full max-w-full"
+                            src={box.main_image.filename}
+                          ></img>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                }
+
+                if (box.type == "k-small") {
+                  return (
+                    <div
+                      className="mr-8 grid min-w-[32rem] max-w-[32rem] grid-cols-3 gap-6"
+                      key={box._uid}
+                    >
+                      <div className="col-span-1">
+                        <img
+                          className="h-full max-w-full"
+                          src={box.main_image.filename}
+                        ></img>
+                      </div>
+                      <div className="col-span-2 flex flex-col items-start justify-end">
+                        <h2 className="h-14 text-lg uppercase">
+                          {box.headline}
+                        </h2>
+                        <h3>{box.subline}</h3>
+                      </div>
+                    </div>
+                  );
+                }
+
                 return (
                   <div
                     className="relative flex h-32 min-w-[30rem] items-center justify-center bg-neutral-800 p-10"
                     key={box._uid}
                   >
-                    <h3>Kontakt Big</h3>
+                    <h3>Bitte setzen sie den richten Typen des Sliders</h3>
                   </div>
                 );
-              }
-
-              if (box.type == "k-small") {
-                return (
-                  <div
-                    className="relative flex h-32 min-w-[30rem] items-center justify-center bg-neutral-800 p-10"
-                    key={box._uid}
-                  >
-                    <h3>Kontakt Small</h3>
-                  </div>
-                );
-              }
-
-              return (
-                <div
-                  className="relative flex h-32 min-w-[30rem] items-center justify-center bg-neutral-800 p-10"
-                  key={box._uid}
-                >
-                  <h3>Bitte setzen sie den richten Typen des Sliders</h3>
-                </div>
-              );
-            })}
-          </motion.div>
+              })}
+            </motion.div>
+          </div>
         </div>
       </div>
     </section>
