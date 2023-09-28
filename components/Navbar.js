6c20673afb5bee13b73b419602741ceb5336905e
P-Klose <ipad@klose.at>
2023-09-28 21:31:01 +0200

    feat(component:Navbar): static Navbar loads in every Page (close #28)

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,102 +1,137 @@
 "use client";
-import { storyblokEditable } from "@storyblok/react/rsc";
+import {
+  apiPlugin,
+  getStoryblokApi,
+  storyblokInit,
+  storyblokEditable,
+} from "@storyblok/react/rsc";
 
-import { useRef } from "react";
+import { useRef, useState, useEffect } from "react"; // Beachte die hinzugefügte useEffect-Importierung
 
 import Link from "next/link";
 import { FaXmark, FaBars } from "react-icons/fa6";
 
-const Navbar = ({ blok }) => {
-  const navRef = useRef();
+const Navbar = () => {
+  const [navbar, setNavbar] = useState();
 
+  storyblokInit({
+    accessToken: process.env.storyblokApiToken,
+    use: [apiPlugin],
+  });
+
+  const navRef = useRef();
   const showNavbar = () => {
     navRef.current.classList.toggle("hidden");
   };
 
-  return (
-    <header {...storyblokEditable(blok)} className="">
-      <nav className="bg-gray-50">
-        <div className="mx-auto max-w-screen-xl">
-          <div className="flex h-14 items-center justify-between p-4">
-            <div className="">
-              <Link className=" text-nav-base" href="/">
-                <img className="w-40" src={blok.logo.filename}></img>
-              </Link>
-            </div>
-            <div className="hidden items-center space-x-4 md:flex">
-              {blok.middle_nav.map((nestedBlok) => {
-                let url = "";
-                if (nestedBlok.link.linktype == "url") {
-                  url = nestedBlok.link.url;
-                } else if (nestedBlok.link.linktype == "story") {
-                  url = "/" + nestedBlok.link.cached_url;
-                }
-                return (
-                  <Link
-                    className={nestedBlok.style}
-                    href={url}
-                    key={nestedBlok._uid}
-                  >
-                    {nestedBlok.display_name}
-                  </Link>
-                );
-              })}
-            </div>
+  useEffect(() => {
+    const getNavbar = async () => {
+      const storyblokApi = getStoryblokApi();
+      const { data } = await fetchData();
+      setNavbar(data.story.content);
+    };
 
-            <div className="hidden items-center md:flex">
-              {/* <Link
+    getNavbar(); // Hier rufst du die Funktion einmalig beim ersten Rendern auf
+
+    // Entferne die Endlosschleife
+  }, []); // Das leere Array als zweites Argument sorgt dafür, dass useEffect nur beim ersten Rendern aufgerufen wird
+
+  if (navbar != undefined) {
+    return (
+      <header {...storyblokEditable(navbar)} className="">
+        <nav className="bg-gray-50">
+          <div className="mx-auto max-w-screen-xl">
+            <div className="flex h-14 items-center justify-between p-4">
+              <div className="">
+                <Link className=" text-nav-base" href="/">
+                  <img className="w-40" src={navbar.logo.filename}></img>
+                </Link>
+              </div>
+              <div className="hidden items-center space-x-4 md:flex">
+                {navbar.middle_nav.map((nestedBlok) => {
+                  let url = "";
+                  if (nestedBlok.link.linktype == "url") {
+                    url = nestedBlok.link.url;
+                  } else if (nestedBlok.link.linktype == "story") {
+                    url = "/" + nestedBlok.link.cached_url;
+                  }
+                  return (
+                    <Link
+                      className={nestedBlok.style}
+                      href={url}
+                      key={nestedBlok._uid}
+                    >
+                      {nestedBlok.display_name}
+                    </Link>
+                  );
+                })}
+              </div>
+
+              <div className="hidden items-center md:flex">
+                {/* <Link
                 className="underline decoration-2 decoration-orange-400 transition duration-300"
                 href="/schueler-innen"
               >
                 Schüler:innen
               </Link> */}
-              {blok.side_nav.map((nestedBlok) => {
-                let url = "";
-                if (nestedBlok.link.linktype == "url") {
-                  url = nestedBlok.link.url;
-                } else if (nestedBlok.link.linktype == "story") {
-                  url = "/" + nestedBlok.link.cached_url;
-                }
-                return (
-                  <Link
-                    className={nestedBlok.style}
-                    href={url}
-                    key={nestedBlok._uid}
-                  >
-                    {nestedBlok.display_name}
-                  </Link>
-                );
-              })}
-            </div>
-            <div className="flex items-center md:hidden">
-              <button onClick={showNavbar}>
-                <FaBars className="h-6 w-6" />
-              </button>
+                {navbar.side_nav.map((nestedBlok) => {
+                  let url = "";
+                  if (nestedBlok.link.linktype == "url") {
+                    url = nestedBlok.link.url;
+                  } else if (nestedBlok.link.linktype == "story") {
+                    url = "/" + nestedBlok.link.cached_url;
+                  }
+                  return (
+                    <Link
+                      className={nestedBlok.style}
+                      href={url}
+                      key={nestedBlok._uid}
+                    >
+                      {nestedBlok.display_name}
+                    </Link>
+                  );
+                })}
+              </div>
+              <div className="flex items-center md:hidden">
+                <button onClick={showNavbar}>
+                  <FaBars className="h-6 w-6" />
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-        <div ref={navRef} className="hidden md:hidden">
-          {blok.middle_nav.map((nestedBlok) => {
-            let url = "";
-            if (nestedBlok.link.linktype == "url") {
-              url = nestedBlok.link.url;
-            } else if (nestedBlok.link.linktype == "story") {
-              url = "/" + nestedBlok.link.cached_url;
-            }
-            return (
-              <Link
-                className="block px-4 py-2 text-sm hover:bg-gray-200"
-                href={url}
-                key={nestedBlok._uid}
-              >
-                {nestedBlok.display_name}
-              </Link>
-            );
-          })}
-        </div>
-      </nav>
-    </header>
-  );
+          <div ref={navRef} className="hidden md:hidden">
+            {navbar.middle_nav.map((nestedBlok) => {
+              let url = "";
+              if (nestedBlok.link.linktype == "url") {
+                url = nestedBlok.link.url;
+              } else if (nestedBlok.link.linktype == "story") {
+                url = "/" + nestedBlok.link.cached_url;
+              }
+              return (
+                <Link
+                  className="block px-4 py-2 text-sm hover:bg-gray-200"
+                  href={url}
+                  key={nestedBlok._uid}
+                >
+                  {nestedBlok.display_name}
+                </Link>
+              );
+            })}
+          </div>
+        </nav>
+      </header>
+    );
+  }
 };
+
+export async function fetchData() {
+  let sbParams = {
+    // cache: "no-store",
+    version: process.env.storyblokApiVersion,
+  };
+
+  const stroyblokApi = getStoryblokApi();
+  return stroyblokApi.get(`cdn/stories/shared-components/navbar`, sbParams);
+}
 
 export default Navbar;
--- components/Page.js
+++ components/Page.js
@@ -1,7 +1,9 @@
 import { storyblokEditable, StoryblokComponent } from "@storyblok/react/rsc";
+import Navbar from "./Navbar";
 
 const Page = ({ blok }) => (
   <main {...storyblokEditable(blok)}>
+    <Navbar></Navbar>
     {blok.body.map((nestedBlok) => (
       <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
     ))}
