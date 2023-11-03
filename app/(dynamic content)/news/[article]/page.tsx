9080df0ca344ab08600ce135408ad514b3b9a361
P-Klose <ipad@klose.at>
2023-11-03 11:26:03 +0100

    fix(Article:sub-News): fix export

--- app/(dynamic content)/news/[article]/page.tsx
+++ app/(dynamic content)/news/[article]/page.tsx
@@ -1,4 +1,6 @@
 import Article from "@/components/Basic/Article";
+import Footer from "@/components/Footer/Footer";
+import Navbar from "@/components/Navbar";
 import { getStoryblokApi, StoryblokComponent } from "@storyblok/react/rsc";
 import { FC } from "react";
 
@@ -10,16 +12,20 @@ const Home: FC<pageProps> = async ({ params }) => {
   const { data } = await fetchData(params.article);
 
   return (
-    <StoryblokComponent
-      blok={data.story.content}
-      key={data.story.content._uid}
-    />
+    <main>
+      <Navbar></Navbar>
+      <StoryblokComponent
+        blok={data.story.content}
+        key={data.story.content._uid}
+      />
+      <Footer></Footer>
+    </main>
   );
 };
 
 export default Home;
 
-export async function fetchData(articlename: string) {
+async function fetchData(articlename: string) {
   let sbParams = {
     // cache: "no-store",
     version: process.env.storyblokApiVersion,
