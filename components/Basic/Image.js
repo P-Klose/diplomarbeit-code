2eef26e5cda3a1c08a4df7a84f2a70c0edae5ac3
P-Klose <ipad@klose.at>
2023-11-03 12:19:53 +0100

    feat(sposoring-small): (close #40)

--- components/Basic/Image.js
+++ components/Basic/Image.js
@@ -1,7 +1,13 @@
 import { storyblokEditable } from "@storyblok/react/rsc";
 
 const ImageComponent = ({ blok }) => {
-  return <img className="h-full max-h-60" src={blok.image.filename}></img>;
+  if (blok.type == "logo-small") {
+    return (
+      <img className="mx-10 h-full max-h-10" src={blok.image.filename}></img>
+    );
+  } else {
+    return <img className="h-full max-h-60" src={blok.image.filename}></img>;
+  }
 };
 
 export default ImageComponent;
