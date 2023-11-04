6c3ac71797e55007ac9e95b4427c225bdfde2663
P-Klose <ipad@klose.at>
2023-11-04 11:40:49 +0100

    feat(Event): add Page and sub-Page rendering (close #60)

---
+++ app/(tabs)/schueler-innen/events/[article]/page.tsx
@@ -0,0 +1,39 @@
+import Article from "@/components/Basic/Article";
+import Footer from "@/components/Footer/Footer";
+import Navbar from "@/components/Navbar";
+import { getStoryblokApi, StoryblokComponent } from "@storyblok/react/rsc";
+import { FC } from "react";
+
+interface pageProps {
+  params: { article: string };
+}
+
+const Home: FC<pageProps> = async ({ params }) => {
+  const { data } = await fetchData(params.article);
+
+  return (
+    <main>
+      <Navbar></Navbar>
+      <StoryblokComponent
+        blok={data.story.content}
+        key={data.story.content._uid}
+      />
+      <Footer></Footer>
+    </main>
+  );
+};
+
+export default Home;
+
+async function fetchData(articlename: string) {
+  let sbParams = {
+    // cache: "no-store",
+    version: process.env.storyblokApiVersion,
+  };
+
+  const stroyblokApi = getStoryblokApi();
+  return stroyblokApi.get(
+    `cdn/stories/schueler-innen/events/${articlename}`,
+    sbParams,
+  );
+}
