2b8196717f580375e028890079bb80e5984f6091
P-Klose <ipad@klose.at>
2023-11-04 10:56:13 +0100

    feat(ValueDokument): add triangle (close #36)

--- components/Custom/ValueDokument.js
+++ components/Custom/ValueDokument.js
@@ -8,22 +8,30 @@ import ValueDokumentEntry from "./ValueDokumentEntry";
 const ValueDokument = ({ blok }) => {
   var content_count = 0;
   return (
-    <motion.div className="mx-auto max-w-screen-2xl">
+    <motion.div className="mx-auto max-w-screen-2xl p-4">
       {/* <div className=" float-right w-full bg-white"></div> */}
       <h2 className="p-4 text-2xl font-semibold">{blok.headline}</h2>
-      <h3 className="">{blok.subline}</h3>
-      <div className="shaped-value-document">
-        {blok.content?.map((nestedBlok) => {
-          content_count = content_count + 1;
+      <h3 className="pl-4">{blok.subline}</h3>
+      <div className="relative h-full">
+        <div className="shaped-value-document">
+          {blok.content?.map((nestedBlok) => {
+            content_count = content_count + 1;
 
-          return (
-            <ValueDokumentEntry
-              key={nestedBlok._uid}
-              blok={nestedBlok}
-              count={content_count}
-            ></ValueDokumentEntry>
-          );
-        })}
+            return (
+              <ValueDokumentEntry
+                key={nestedBlok._uid}
+                blok={nestedBlok}
+                count={content_count}
+              ></ValueDokumentEntry>
+            );
+          })}
+        </div>
+        <div
+          className="shaped-value-document-overlay absolute top-0 hidden h-full w-full sm:block"
+          style={{
+            background: `linear-gradient(rgba(0,0, 0, 0.6),rgba(0, 0, 0, 0.3)),url(${blok.background_image.filename}) no-repeat bottom center / cover`,
+          }}
+        ></div>
       </div>
     </motion.div>
   );
