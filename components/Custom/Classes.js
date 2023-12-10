11bc039b5132b894a891c0d3910720a938111c46
P-Klose <ipad@klose.at>
2023-12-10 18:57:46 +0100

    feat(Component: Classes): add new component (close #35)

---
+++ components/Custom/Classes.js
@@ -0,0 +1,77 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { storyblokEditable, StoryblokComponent } from "@storyblok/react/rsc";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+
+// import { storyblokInit, apiPlugin } from "@storyblok/react/rsc";
+
+const ClassesOverview = ({ blok }) => {
+  const searchParams = useSearchParams();
+  const selectedClassStr = searchParams.get("class");
+  var selectedClass = blok.classes.find(
+    (subject) => subject.classname.toLowerCase() === selectedClassStr,
+  );
+  if (selectedClass == undefined) {
+    selectedClass = blok.classes[0];
+  }
+  var otherClasses = [];
+  var index = blok.classes.indexOf(selectedClass);
+  var length = blok.classes.length - 1;
+  if (index == 0) {
+    otherClasses.push(blok.classes[++index]);
+    otherClasses.push(blok.classes[length]);
+  } else if (index == length) {
+    otherClasses.push(blok.classes[0]);
+    otherClasses.push(blok.classes[--index]);
+  } else {
+    otherClasses.push(blok.classes[index + 1]);
+    otherClasses.push(blok.classes[index - 1]);
+  }
+
+  return (
+    <motion.div
+      {...storyblokEditable(blok)}
+      className="mx-auto max-w-screen-2xl p-4"
+    >
+      <h2 className="p-4 pb-3 text-3xl font-semibold uppercase">
+        {blok.headline}
+      </h2>
+      <div className="flex flex-row">
+        {blok.classes?.map((nestedBlok) => {
+          return (
+            <Link
+              href={`?class=${nestedBlok.classname.toLowerCase()}`}
+              className="m-4 uppercase"
+              key={nestedBlok._uid}
+            >
+              {nestedBlok.classname}
+            </Link>
+          );
+        })}
+      </div>
+      <div className="grid grid-cols-2 gap-4 md:grid-cols-3">
+        <img
+          className="col-span-2 row-span-2 md:col-span-2"
+          src={selectedClass.img.filename}
+          alt={`Klassenfoto der ${selectedClass.classname}`}
+        ></img>
+        {otherClasses.map((nestedClass) => {
+          return (
+            <Link
+              className="col-span-1 self-stretch bg-neutral-100"
+              href={`?class=${nestedClass.classname.toLowerCase()}`}
+              key={nestedClass._uid}
+            >
+              <div className="flex h-full items-center justify-center p-6 text-xl font-medium uppercase">
+                {nestedClass.classname}
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+    </motion.div>
+  );
+};
+export default ClassesOverview;
