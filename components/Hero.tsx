5efc73a0cc2bc4d7afac3c30fb5956fbf43b2eeb
P-Klose <ipad@klose.at>
2024-01-01 17:07:31 +0100

    feat(component: Hero): first implementation of skew

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -14,25 +14,23 @@ const Hero = ({ blok }) => {
   return (
     <section
       {...storyblokEditable(blok)}
-      className={`mx-auto grid h-auto max-w-screen-2xl sm:grid-cols-7 ${
+      className={`relative mx-auto grid h-auto max-w-screen-2xl sm:grid-cols-7 ${
         blok.themeisdark ? "bg-zinc-900" : "bg-white"
       }`}
     >
       <div
         className={`flex h-32 sm:${blok.height} ${
           blok.fixed ? "md:sticky md:top-0" : ""
-        } items-center justify-start overflow-hidden bg-cover bg-no-repeat ${
-          blok.type
+        } items-center justify-start overflow-hidden bg-cover bg-no-repeat  ${
+          blok.type == "school"
+            ? `${blok.type} mb-2 md:mb-24 lg:mb-12 xl:mb-8`
+            : `${blok.type}`
         }`}
         style={{
           background: `linear-gradient(rgba(0,0, 0, 0.6),rgba(0, 0, 0, 0.3)),url(${blok.background_image.filename}) no-repeat bottom center / cover`,
         }}
       >
-        <h1
-          className="w-full text-center text-5xl font-extrabold text-white sm:ml-8 sm:w-1/2 sm:text-left md:ml-16 md:text-7xl xl:text-9xl"
-          // {...storyblokEditable(blok)}
-          //ml-4
-        >
+        <h1 className="w-full text-center text-5xl font-extrabold text-white sm:ml-8 sm:w-1/2 sm:text-left md:ml-16 md:text-7xl xl:text-9xl">
           {blok.headline}
         </h1>
       </div>
@@ -40,14 +38,12 @@ const Hero = ({ blok }) => {
         <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
       ))}
       {blok.type == "school" ? (
-        <div className="relative -top-8 xs:-top-10 sm:-top-12 sm:col-span-7 md:-top-16">
-          <img
-            // src={process.env.PUBLIC_URL + "/schule_weiss.png"}
-            src="./schule_weiss.png"
-            alt="schule in schwarz weiß"
-            className="mx-auto w-full max-w-screen-xl p-4"
-          />
-        </div>
+        <img
+          // src={process.env.PUBLIC_URL + "/schule_weiss.png"}
+          src="./schule_weiss.png"
+          alt="schule in schwarz weiß"
+          className="absolute -bottom-6 md:-right-[30%] md:bottom-0 md:w-2/3 lg:-right-[15%] lg:bottom-2 lg:w-2/3 xl:-bottom-2 xl:-right-[27%] xl:w-2/3"
+        />
       ) : null}
     </section>
   );
