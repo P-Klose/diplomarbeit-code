5d27266dbc727fa444bd431374285247adf09591
P-Klose <ipad@klose.at>
2024-01-02 11:27:52 +0100

    fix(component: Headline): consistent padding

--- components/Basic/Headline.tsx
+++ components/Basic/Headline.tsx
@@ -4,9 +4,9 @@ const Headline = ({ blok }) => {
   return (
     <div
       {...storyblokEditable(blok)}
-      className="col-span-full mx-auto w-full max-w-screen-2xl bg-white"
+      className="col-span-full mx-auto w-full max-w-screen-2xl bg-white p-4"
     >
-      <h2 className="p-4 pb-3 text-2xl font-semibold uppercase md:text-3xl">
+      <h2 className="pl-4 text-2xl font-semibold uppercase md:text-3xl">
         {blok.headline}
       </h2>
     </div>
