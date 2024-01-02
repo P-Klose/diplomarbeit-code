ec5e2aafd4f90afc69b74164efb156632d16e048
P-Klose <ipad@klose.at>
2024-01-02 09:45:48 +0100

    feat(component: Hero): adapt Ausbildung with school to hero skew

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -16,21 +16,20 @@ const Hero = ({ blok }) => {
       {...storyblokEditable(blok)}
       className={`relative mx-auto grid h-auto max-w-screen-2xl sm:grid-cols-7 ${
         blok.themeisdark ? "bg-zinc-900" : "bg-white"
-      }`}
+      }
+      ${blok.type == "school" ? "sm:mb-10 md:mb-14 lg:mb-20 xl:mb-40" : ""}`}
     >
       <div
         className={`flex h-32 sm:${blok.height} ${
           blok.fixed ? "md:sticky md:top-0" : ""
-        } items-center justify-start overflow-hidden bg-cover bg-no-repeat  ${
-          blok.type == "school"
-            ? `${blok.type} mb-2 md:mb-24 lg:mb-12 xl:mb-8`
-            : `${blok.type}`
+        } items-center justify-start overflow-hidden bg-cover bg-no-repeat ${
+          blok.type
         }`}
         style={{
           background: `linear-gradient(rgba(0,0, 0, 0.6),rgba(0, 0, 0, 0.3)),url(${blok.background_image.filename}) no-repeat bottom center / cover`,
         }}
       >
-        <h1 className="w-full text-center text-5xl font-extrabold text-white sm:ml-8 sm:w-1/2 sm:text-left md:ml-16 md:text-7xl xl:text-9xl">
+        <h1 className="w-full text-center text-5xl font-extrabold text-white sm:ml-8 sm:w-1/2 sm:text-left md:ml-16 md:text-7xl xl:text-8xl">
           {blok.headline}
         </h1>
       </div>
@@ -42,7 +41,7 @@ const Hero = ({ blok }) => {
           // src={process.env.PUBLIC_URL + "/schule_weiss.png"}
           src="./schule_weiss.png"
           alt="schule in schwarz weiß"
-          className="absolute -bottom-6 md:-right-[30%] md:bottom-0 md:w-2/3 lg:-right-[15%] lg:bottom-2 lg:w-2/3 xl:-bottom-2 xl:-right-[27%] xl:w-2/3"
+          className="absolute left-0 hidden w-2/3 p-4 sm:-bottom-10 sm:block md:-bottom-14 lg:-bottom-24 xl:-bottom-40"
         />
       ) : null}
     </section>
