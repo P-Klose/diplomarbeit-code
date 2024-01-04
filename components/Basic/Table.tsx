1d57ce6bbe57fe5bf8b1267f367fd441cff08623
P-Klose <ipad@klose.at>
2024-01-04 20:47:42 +0100

    fix(Component: Table): defined key

--- components/Basic/Table.tsx
+++ components/Basic/Table.tsx
@@ -10,15 +10,16 @@ const Table = ({ blok }) => {
       )}
       <table
         className="mb-4 w-full table-fixed xs:mx-4"
+        key={blok._uid}
         {...storyblokEditable(blok)}
       >
         <thead className="mb-4  ">
           <tr>
-            {blok.table?.thead?.map((th: any) => {
+            {blok.table?.thead?.map((th: any, th_index: number) => {
               return (
                 <th
                   className="px-2 py-2 text-left text-lg font-semibold md:text-xl"
-                  key={th._uid}
+                  key={`${th_index}`}
                 >
                   {th.value}
                 </th>
@@ -30,14 +31,16 @@ const Table = ({ blok }) => {
           className="divide-y-2"
           //className="[&>*:nth-child(even)]:bg-neutral-50 [&>*:nth-child(odd)]:bg-neutral-100"
         >
-          {blok.table?.tbody?.map((tr: any) => {
+          {blok.table?.tbody?.map((tr: any, tr_index: number) => {
+            // console.log(tr);
+
             return (
-              <tr key={tr._uid}>
-                {tr.body?.map((td: any) => {
+              <tr key={tr_index}>
+                {tr.body?.map((td: any, te_index: number) => {
                   return (
                     <th
                       className="px-2 py-1 text-left text-lg font-normal"
-                      key={td._uid}
+                      key={`${tr_index}_${te_index}`}
                     >
                       {td.value}
                     </th>
