7e34d0172ca614c1c88e43814fc12c0b8f6a56fc
P-Klose <ipad@klose.at>
2024-01-05 21:40:40 +0100

    fix(Component: LinkCollection): max-w on larger screens

--- components/Basic/LinkCollection.tsx
+++ components/Basic/LinkCollection.tsx
@@ -5,7 +5,10 @@ const LinkCollection: React.FC<{ blok: LinkCollectionProps }> = ({ blok }) => {
   const grid_width = ["col-span-1", "col-span-2", "col-span-3", "col-span-4"];
 
   return (
-    <section {...storyblokEditable(blok)} className="grid max-w-screen-2xl p-4">
+    <section
+      {...storyblokEditable(blok)}
+      className="mx-auto grid max-w-screen-2xl p-4"
+    >
       <h2 className="break-words text-2xl font-semibold uppercase md:text-3xl">
         {blok.headline}
       </h2>
