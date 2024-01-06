ba69a123c24fa156e6e8b932375e1aa25fdefde5
P-Klose <ipad@klose.at>
2024-01-06 08:43:09 +0100

    feat(FAQs): text done video missing

--- components/Custom/FaqCollectionElement.tsx
+++ components/Custom/FaqCollectionElement.tsx
@@ -1,17 +1,42 @@
 import { storyblokEditable, StoryblokComponent } from "@storyblok/react/rsc";
 import { FaqProps } from "@/types/interfaces";
 import { render } from "storyblok-rich-text-react-renderer";
+import { LayoutGroup } from "framer-motion";
+import { motion } from "framer-motion";
+import { useState } from "react";
 
 const FaqElement: React.FC<{ blok: FaqProps }> = ({ blok }) => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="grid grid-cols-1 gap-1" {...storyblokEditable(blok)}>
-      <p className="border-b-2 border-black py-2 text-lg font-medium md:px-2 md:text-xl">
+    <motion.div
+      layout="position"
+      className="grid cursor-pointer grid-cols-1"
+      {...storyblokEditable(blok)}
+      onClick={() => setIsOpen(!isOpen)}
+    >
+      <motion.p
+        layout="position"
+        className={`border-b-2  ${
+          isOpen ? "bg-neutral-100" : "bg-white"
+        } border-black py-2 text-lg font-medium transition-colors hover:bg-neutral-100 md:px-2 md:text-xl`}
+      >
         {blok.question}
-      </p>
-      <div className="prose max-w-none prose-p:text-base md:px-2">
-        {render(blok.answer)}
-      </div>
-    </div>
+      </motion.p>
+      <LayoutGroup>
+        {isOpen ? (
+          <motion.div
+            layout="position"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+            onClick={() => setIsOpen(!isOpen)}
+            className="prose max-w-none bg-neutral-100 py-2 prose-p:text-base prose-p:text-neutral-900 md:px-2"
+          >
+            {render(blok.answer)}
+          </motion.div>
+        ) : null}
+      </LayoutGroup>
+    </motion.div>
   );
 };
 export default FaqElement;
