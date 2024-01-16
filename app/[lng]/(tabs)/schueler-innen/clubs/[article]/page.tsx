3f7ba7dcc18d4b74f3ffff43176c9b8e347d3ee2
P-Klose <ipad@klose.at>
2024-01-16 22:58:35 +0100

    feat(Clubs): add Clubs and Club overview TODO: club in allArticles and 'mehr erfahren' link

---
+++ app/[lng]/(tabs)/schueler-innen/clubs/[article]/page.tsx
@@ -0,0 +1,30 @@
+import { ISbStoriesParams, getStoryblokApi } from "@storyblok/react/rsc";
+import StoryblokStory from "@storyblok/react/story";
+import { FC } from "react";
+
+interface pageProps {
+  params: { article: string; lng: string };
+}
+
+const Home: FC<pageProps> = async ({ params }) => {
+  const { data } = await fetchData(params.article, params.lng);
+
+  return <StoryblokStory story={data.story} key={data.story.content._uid} />;
+};
+
+export default Home;
+
+async function fetchData(articlename: string, lng: string) {
+  let sbParams: ISbStoriesParams = {
+    // cache: "no-store",
+    version:
+      process.env.storyblokApiVersion == "published" ? "published" : "draft",
+    language: lng,
+  };
+
+  const stroyblokApi = getStoryblokApi();
+  return stroyblokApi.get(
+    `cdn/stories/schueler-innen/clubs/${articlename}`,
+    sbParams,
+  );
+}
---
+++ app/[lng]/(tabs)/schueler-innen/clubs/page.tsx
@@ -0,0 +1,33 @@
+import { PageProps } from "@/types/interfaces";
+import {
+  getStoryblokApi,
+  ISbStoriesParams,
+  StoryblokComponent,
+} from "@storyblok/react/rsc";
+import StoryblokStory from "@storyblok/react/story";
+import { Metadata } from "next";
+import { FC } from "react";
+
+export const metadata: Metadata = {
+  title: "Events - HTL Leonding",
+  description: "",
+};
+
+const Home: FC<PageProps> = async ({ params }) => {
+  const { data } = await fetchData(params.lng);
+  return <StoryblokStory story={data.story} />;
+};
+
+export default Home;
+
+async function fetchData(lng: string) {
+  let sbParams: ISbStoriesParams = {
+    // cache: "no-store",
+    version:
+      process.env.storyblokApiVersion == "published" ? "published" : "draft",
+    language: lng,
+  };
+
+  const stroyblokApi = getStoryblokApi();
+  return stroyblokApi.get(`cdn/stories/schueler-innen/clubs`, sbParams);
+}
