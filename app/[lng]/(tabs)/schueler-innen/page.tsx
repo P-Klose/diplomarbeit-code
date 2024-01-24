a97b78c1f18a11f8d4f524b94edbff846b6f7ca0
P-Klose <ipad@klose.at>
2024-01-24 21:07:17 +0100

    feat(Deployment): new storyblok design testing

--- app/[lng]/(tabs)/schueler-innen/page.tsx
+++ app/[lng]/(tabs)/schueler-innen/page.tsx
@@ -1,4 +1,5 @@
 import { PageProps } from "@/types/interfaces";
+import { fetchData } from "@/util/storyblok";
 import { ISbStoriesParams, getStoryblokApi } from "@storyblok/react/rsc";
 import StoryblokStory from "@storyblok/react/story";
 import { Metadata } from "next";
@@ -10,22 +11,8 @@ export const metadata: Metadata = {
 };
 
 const Home: FC<PageProps> = async ({ params }) => {
-  const { data } = await fetchData(params.lng);
+  const data = await fetchData(params.lng, "schueler-innen");
   return <StoryblokStory story={data.story} />;
 };
 
 export default Home;
-
-async function fetchData(lng: string) {
-  let sbParams: ISbStoriesParams = {
-    // cache: "no-store",
-    version:
-      process.env.storyblokApiVersion == "published" ? "published" : "draft",
-    resolve_relations: ["featured_articles.articles"],
-    cv: Date.now(),
-    language: lng,
-  };
-
-  const stroyblokApi = getStoryblokApi();
-  return stroyblokApi.get(`cdn/stories/schueler-innen`, sbParams);
-}
---
+++ util/storyblok.tsx
@@ -0,0 +1,21 @@
+"server only";
+
+import { ISbStoriesParams, getStoryblokApi } from "@storyblok/react/rsc";
+
+export async function fetchData(lng: string, slug: string) {
+  const stroyblokApi = getStoryblokApi();
+  let sbParams: ISbStoriesParams = {
+    // cache: "no-store",
+    version:
+      process.env.storyblokApiVersion == "published" ? "published" : "draft",
+    resolve_relations: [
+      "featured_articles.articles",
+      "scroll_slider_select.slider",
+    ],
+    cv: Date.now(),
+    language: lng,
+  };
+  const { data } = await stroyblokApi.get(`cdn/stories/${slug}`, sbParams);
+
+  return data;
+}
