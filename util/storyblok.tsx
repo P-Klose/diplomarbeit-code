10e17cc6dbf2c1e46cf4a0643f34b9a94b1ff5f1
P-Klose <ipad@klose.at>
2024-01-25 11:57:32 +0100

    feat(fetch): next revalidate

--- util/storyblok.tsx
+++ util/storyblok.tsx
@@ -1,82 +1,72 @@
 import { ISbStoriesParams, getStoryblokApi } from "@storyblok/react/rsc";
 import { revalidateTag } from "next/cache";
 
-export async function fetchData(lng: string, slug: string) {
-  const stroyblokApi = getStoryblokApi();
-  let sbParams: ISbStoriesParams = {
-    // cache: "no-store",
-    cv: Date.now(),
-    version:
-      process.env.storyblokApiVersion == "published" ? "published" : "draft",
-    resolve_relations: [
-      "featured_articles.articles",
-      "scroll_slider_select.slider",
-    ],
-    language: lng,
-  };
-  const { data } = await stroyblokApi.get(`cdn/stories/${slug}`, sbParams);
-
-  return data;
-}
-
 // export async function fetchData(lng: string, slug: string) {
-//   const storyblokApiBaseUrl = "https://api.storyblok.com/v2/cdn/stories/";
-//   const storyblokToken = process.env.storyblokApiToken; // replace with your actual Storyblok token
-//   const version =
-//     process.env.storyblokApiVersion === "published" ? "published" : "draft";
-
-//   // working but logic for resolveRelations missing bc relations are now on the top level of the json als rels and not where they are located normaly
-
-//   const resolveRelations = [
-//     "featured_articles.articles",
-//     "scroll_slider_select.slider",
-//   ];
-
-//   const queryString = new URLSearchParams({
-//     resolve_relations: resolveRelations.join(","),
-//     version: version,
+//   const stroyblokApi = getStoryblokApi();
+//   let sbParams: ISbStoriesParams = {
+//     // cache: "no-store",
+//     cv: Date.now(),
+//     version:
+//       process.env.storyblokApiVersion == "published" ? "published" : "draft",
+//     resolve_relations: [
+//       "featured_articles.articles",
+//       "scroll_slider_select.slider",
+//     ],
 //     language: lng,
-//   });
-
-//   const apiUrl = `${storyblokApiBaseUrl}${slug}?${queryString.toString()}&token=${storyblokToken}`;
-
-//   // console.log(apiUrl);
-
-//   const response = await fetch(apiUrl, {
-//     method: "GET",
-//     headers: {
-//       Accept: "application/json",
-//       "Content-Type": "application/json",
-//     },
-//   });
-//   const data = await response.json();
-
-//   return updateArticlesWithContent(data);
-// }
-
-// function updateArticlesWithContent(data: any): any {
-//   const relsMap: Record<string, any> = {};
-
-//   // Build a mapping of rel UUIDs to their content
-//   data.rels.forEach((rel: any) => {
-//     relsMap[rel.uuid] = rel.content;
-//   });
-
-//   // Update the articles array with correct content
-//   data.story.content.body.forEach((section: any) => {
-//     if (section.articles) {
-//       section.articles = section.articles.map(
-//         (articleUUID: string) => relsMap[articleUUID],
-//       );
-//     }
-//     if (section.articles) {
-//       section.articles = section.articles.map(
-//         (articleUUID: string) => relsMap[articleUUID],
-//       );
-//     }
-//   });
-
-//   console.log(data.story.content.body);
+//   };
+//   const { data } = await stroyblokApi.get(`cdn/stories/${slug}`, sbParams);
 
 //   return data;
 // }
+
+export async function fetchData(lng: string, slug: string) {
+  const storyblokApiBaseUrl = "https://api.storyblok.com/v2/cdn/stories/";
+  const storyblokToken = process.env.storyblokApiToken; // replace with your actual Storyblok token
+  const version =
+    process.env.storyblokApiVersion === "published" ? "published" : "draft";
+
+  const resolveRelations = [
+    "featured_articles.articles",
+    "scroll_slider_select.slider",
+  ];
+  const queryString = new URLSearchParams({
+    resolve_relations: resolveRelations.join(","),
+    version: version,
+    language: lng,
+  });
+  const apiUrl = `${storyblokApiBaseUrl}${slug}?${queryString.toString()}&token=${storyblokToken}`;
+
+  const response = await fetch(apiUrl, {
+    method: "GET",
+    next: { revalidate: 10 },
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
+  const data = await response.json();
+  return updateArticlesWithContent(data);
+}
+
+function updateArticlesWithContent(data: any): any {
+  const relsMap: Record<string, any> = {};
+
+  data.rels.forEach((rel: any) => {
+    relsMap[rel.uuid] = rel;
+  });
+
+  data.story.content.body.forEach((section: any) => {
+    if (section.articles) {
+      section.articles = section.articles.map(
+        (articleUUID: string) => relsMap[articleUUID],
+      );
+    }
+    if (section.slider && section.slider.component == "scroll_slider_select") {
+      section.slider = section.slider.map(
+        (sliderUUID: string) => relsMap[sliderUUID],
+      );
+    }
+  });
+
+  return data;
+}
